baf6c56b37ad1644b6581de764f727d9d62331dd
violettab <violettabatsura@gmail.com>
2025-05-04 21:59:04 +0300

    feat: set up development environment configuration and project structure

---
+++ webpack.common.js
@@ -0,0 +1,87 @@
+const path = require('path');
+
+const devtool = 'source-map';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+module.exports = {
+    devtool,
+    devServer: {
+        open: true,
+        historyApiFallback: true,
+    },
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.[contenthash].css',
+        }),
+        new ESLintPlugin({ extensions: ['ts', 'tsx'] }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: false,
+                        },
+                    },
+                    'sass-loader',
+                ],
+                exclude: /\.module\.scss$/,
+            },
+            {
+                test: /\.(sa|sc)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                mode: 'local',
+                                exportOnlyLocals: false,
+                                namedExport: false,
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+                include: /\.module\.scss$/,
+            },
+            {
+                test: /\.jpe?g$|\.svg$|\.png$|\.ico$|\.mp3$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+};
