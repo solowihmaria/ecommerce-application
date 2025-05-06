75490e9f0a1429465acbb62e54dd6def5c4171db
solowihmaria <solowihmaria@gmail.com>
2025-05-06 21:29:10 +0300

    feat: implement NotFound page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Heading from '../../components/ui/Heading/Heading';
+import Button from '../../components/ui/Button/Button';
+import styles from './NotFound.module.scss';
+
+const NotFoundPage: React.FC = () => {
+    return (
+        <div className={styles.notFound}>
+            <div className={styles.content}>
+                <div className={styles.errorCode}>404</div>
+                <Heading level="h1" className={styles.title}>
+                    Page Not Found
+                </Heading>
+                <p className={styles.message}>
+                    Oops! The page you&apos;re looking for doesn&apos;t exist or
+                    has been moved.
+                </p>
+                <Link to="/" className={styles.homeLink}>
+                    <Button className={styles.button}>Go to Homepage</Button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default NotFoundPage;
