a18166e860177a3d87952eecc512aeaf8851f563
solowihmaria <solowihmaria@gmail.com>
2025-05-06 21:12:25 +0300

    feat: implement base Form component structure

---
+++ src/components/ui/Form/Form.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import type { FormProps } from './Form.types';
+import styles from './Form.module.scss';
+
+const Form = ({
+    children,
+    className = '',
+    ...props
+}: FormProps): React.JSX.Element => {
+    return (
+        <form className={`${styles.form} ${className}`} {...props}>
+            {children}
+        </form>
+    );
+};
+
+export default Form;
