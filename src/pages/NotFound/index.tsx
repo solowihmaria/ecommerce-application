8e5e289447ebf057ed68228ea131835f87cf7fdf
solowihmaria <solowihmaria@gmail.com>
2025-05-06 15:06:43 +0300

    feat: add basic NotFound page template

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const NotFoundPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Страница не найдена</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
