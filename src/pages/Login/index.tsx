cff65dd3f960d902f5f4bb7a9d4d25226c3bb3c6
solowihmaria <solowihmaria@gmail.com>
2025-05-06 15:04:02 +0300

    feat: add basic Login page template

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const LoginPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Страница входа</h1>
+        </div>
+    );
+};
+
+export default LoginPage;
