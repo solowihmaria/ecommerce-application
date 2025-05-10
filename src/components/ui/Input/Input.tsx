db76d0b62932db5599bd565339b8b2a2718c4c9d
solowihmaria <solowihmaria@gmail.com>
2025-05-10 12:26:53 +0300

    feat: enhance Input component with dynamic password visibility

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Input.module.scss';
 import type { InputProps } from './Input.types';
+import { InputToggleButton } from './InputToggle';
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
     (
@@ -9,26 +10,37 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             className = '',
             error = false,
             errorMessage,
+            hasPasswordToggle = false,
+            onTogglePassword,
+            showPassword,
             ...props
         },
         reference
-    ) => {
-        return (
-            <div className={styles.inputContainer}>
+    ) => (
+        <div className={styles.inputContainer}>
+            <div className={styles.inputWrapper}>
                 <input
                     ref={reference}
-                    type={type}
+                    type={showPassword ? 'text' : type}
                     className={`${styles.input} ${error ? styles.error : ''} ${className}`}
                     {...props}
                 />
-                {error && errorMessage && (
-                    <span className={styles.errorMessage}>{errorMessage}</span>
-                )}
+                {hasPasswordToggle &&
+                    type === 'password' &&
+                    onTogglePassword && (
+                        <InputToggleButton
+                            showPassword={!!showPassword}
+                            onTogglePassword={onTogglePassword}
+                        />
+                    )}
             </div>
-        );
-    }
+            {error && (
+                <span className={styles.errorMessage}>{errorMessage}</span>
+            )}
+        </div>
+    )
 );
 
-Input.displayName = 'Input'; // Для отладки в DevTools (пишут, что так удобнее)
+Input.displayName = 'Input';
 
 export default Input;
