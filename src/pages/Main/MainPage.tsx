da7bd8735321db47cf0cc3f9b5865f827000a26e
solowihmaria <solowihmaria@gmail.com>
2025-05-28 13:30:16 +0300

    feat: update MainPage

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,16 +1,10 @@
 import { Header } from '../../components/blocks/Header/Header';
+import { Main } from '../../components/blocks/Main';
 import styles from './MainPage.module.scss';
 
 export const MainPage = () => (
     <div data-testid="main-page-test-id" className={styles.mainPageContainer}>
         <Header />
-        <main className={styles.main}>
-            <div className={styles.titleWrapper}>
-                <h1 className={styles.title}>Evergreen</h1>
-                <h3 className={styles.titleSecondary}>
-                    Find your perfect plant
-                </h3>
-            </div>
-        </main>
+        <Main />
     </div>
 );
