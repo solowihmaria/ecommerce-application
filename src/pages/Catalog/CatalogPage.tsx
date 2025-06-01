166fc28c20c22a128b63a2e911ad6de37d56623d
alesha <co.rin@mail.ru>
2025-06-01 17:43:48 +0300

    feat: add footer to the catalog page

--- src/pages/Catalog/CatalogPage.tsx
+++ src/pages/Catalog/CatalogPage.tsx
@@ -1,24 +1,17 @@
-// import { requestGetProducts } from '../../api/getProducts/requestGetProducts';
 import { Catalog } from '../../components/blocks/Catalog';
+import { Footer } from '../../components/blocks/Footer';
 import { Header } from '../../components/blocks/Header/Header';
-// import { Button } from '../../components/ui/Button';
 import styles from './CatalogPage.module.scss';
 
 export const CatalogPage = () => (
     <div className={styles.catalogPage}>
         <Header />
+
         <main className={styles.catalogContent}>
             <h1 className={styles.title}>Catalog</h1>
-
-            {/* <Button
-                onClick={() => {
-                    void requestGetProducts();
-                }}
-            >
-                Get Products
-            </Button> */}
-
             <Catalog></Catalog>
         </main>
+
+        <Footer />
     </div>
 );
