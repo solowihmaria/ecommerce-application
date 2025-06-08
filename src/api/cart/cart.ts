df533884b4fcb397f49a60ba226ac0ba389393f7
violettab <violettabatsura@gmail.com>
2025-06-08 22:54:44 +0300

    feat: implement cart item delete

--- src/api/cart/cart.ts
+++ src/api/cart/cart.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import type { CartResponse, CustomCart } from './cart.types';
+import type {
+    CartResponse,
+    CustomCart,
+    DeleteItemPayload,
+    QtyUpdatePayload,
+} from './cart.types';
 import { getToken } from '../token';
 
 export const getUserToken = () => {
@@ -47,17 +52,31 @@ export const getUserCart = async (): Promise<CartResponse> => {
     return cart.data;
 };
 
-export const changeItemQty = async (
-    qty: number,
-    cartContent: CustomCart,
-    lineId: string
+export const updateCart = async (
+    payload: QtyUpdatePayload | DeleteItemPayload,
+    cartId: string
 ): Promise<CartResponse> => {
     const token = getUserToken();
 
     const apiUrl = process.env.CTP_API_URL;
     const projectKey = process.env.CTP_PROJECT_KEY;
 
-    const payload = {
+    const cartUrl = `${apiUrl}/${projectKey}/me/carts/${cartId}`;
+    const cart = await axios.post<CartResponse>(cartUrl, payload, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    });
+    return cart.data;
+};
+
+export const changeItemQty = async (
+    qty: number,
+    cartContent: CustomCart,
+    lineId: string
+): Promise<CartResponse> => {
+    const payload: QtyUpdatePayload = {
         version: cartContent.version,
         actions: [
             {
@@ -68,12 +87,24 @@ export const changeItemQty = async (
         ],
     };
 
-    const cartUrl = `${apiUrl}/${projectKey}/me/carts/${cartContent.id}`;
-    const cart = await axios.post<CartResponse>(cartUrl, payload, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-        },
-    });
-    return cart.data;
+    const cart = await updateCart(payload, cartContent.id);
+    return cart;
+};
+
+export const removeCartItem = async (
+    cartContent: CustomCart,
+    lineId: string
+): Promise<CartResponse> => {
+    const payload: DeleteItemPayload = {
+        version: cartContent.version,
+        actions: [
+            {
+                action: 'removeLineItem',
+                lineItemId: lineId,
+            },
+        ],
+    };
+
+    const cart = await updateCart(payload, cartContent.id);
+    return cart;
 };
