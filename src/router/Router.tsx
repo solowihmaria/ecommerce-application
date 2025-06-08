d3fdfb9366f6a02a08affd5cd47d8068db9df7ac
solowihmaria <solowihmaria@gmail.com>
2025-06-08 12:13:51 +0300

    feat: add Outlet for nested route and update all route configurations

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import { MainPage } from '../pages/Main';
 import { LoginPage } from '../pages/Login';
 import { RegistrationPage } from '../pages/Registration';
@@ -7,53 +7,69 @@ import { RedirectIfAuth } from './guards/RedirectIfAuth';
 import { CatalogPage } from '../pages/Catalog';
 import { ProductPage } from '../pages/Product';
 import { ProfilePage } from '../pages/Profile';
+import { BasketPage } from '../pages/Basket';
 import { RequireAuth } from './guards/RequireAuth';
+import { Layout } from '../components/blocks/Layout/Layout';
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: <MainPage />,
-    },
-    {
-        path: '/main',
-        element: <MainPage />,
-    },
-    {
-        path: '/login',
         element: (
-            <RedirectIfAuth>
-                <LoginPage />
-            </RedirectIfAuth>
+            <Layout>
+                <Outlet />{' '}
+            </Layout>
         ),
+        children: [
+            {
+                path: '/main',
+                element: <MainPage />,
+            },
+            {
+                path: '/',
+                element: <MainPage />,
+            },
+            {
+                path: '/login',
+                element: (
+                    <RedirectIfAuth>
+                        <LoginPage />
+                    </RedirectIfAuth>
+                ),
+            },
+            {
+                path: '/register',
+                element: (
+                    <RedirectIfAuth>
+                        <RegistrationPage />
+                    </RedirectIfAuth>
+                ),
+            },
+            {
+                path: '/catalog/:slug?',
+                element: <CatalogPage />,
+            },
+            {
+                path: '/catalog/:categorySlug/:subcategorySlug',
+                element: <CatalogPage />,
+            },
+            {
+                path: '/product/:id',
+                element: <ProductPage />,
+            },
+            {
+                path: '/profile',
+                element: (
+                    <RequireAuth>
+                        <ProfilePage />
+                    </RequireAuth>
+                ),
+            },
+            {
+                path: '/basket',
+                element: <BasketPage />,
+            },
+        ],
     },
-    {
-        path: '/register',
-        element: (
-            <RedirectIfAuth>
-                <RegistrationPage />
-            </RedirectIfAuth>
-        ),
-    },
-    {
-        path: '/catalog/:slug?', // используем slug вместо id для навигации по категориям
-        element: <CatalogPage />,
-    },
-    {
-        path: '/catalog/:categorySlug/:subcategorySlug', // используем slug вместо id для навигации по категориям
-        element: <CatalogPage />,
-    },
-    {
-        path: '/product/:id',
-        element: <ProductPage />,
-    },
-    {
-        path: '/profile',
-        element: (
-            <RequireAuth>
-                <ProfilePage />
-            </RequireAuth>
-        ),
-    },
+
     {
         path: '*',
         element: <NotFoundPage />,
