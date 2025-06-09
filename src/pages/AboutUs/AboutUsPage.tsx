de6bad324d4fa279dabaac7b487c36001dbc142f
solowihmaria <solowihmaria@gmail.com>
2025-06-09 21:43:47 +0300

    feat: add new tests

--- src/pages/AboutUs/AboutUsPage.tsx
+++ src/pages/AboutUs/AboutUsPage.tsx
@@ -2,7 +2,7 @@ import { TeamIntro } from '../../components/blocks/TeamIntro';
 import styles from './AboutUsPage.module.scss';
 
 export const AboutUsPage = () => (
-    <div className={styles.aboutUsPageContainer}>
+    <div data-testid="about-us-page" className={styles.aboutUsPageContainer}>
         <TeamIntro />
     </div>
 );
